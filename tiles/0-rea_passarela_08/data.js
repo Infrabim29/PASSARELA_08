d4893961ec7ee761372f05fde35f7287927268ed
Infrabim29 <infrabim@outlook.com>
2025-03-11 10:44:49 -0300

    Add files via upload

---
+++ tiles/0-rea_passarela_08/data.js
@@ -0,0 +1,42 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-rea_passarela_08",
+      "name": "ÁREA_PASSARELA_08",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.41357868579195056,
+        "pitch": 1.5707963267948966,
+        "fov": 1.3473765980835988
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
